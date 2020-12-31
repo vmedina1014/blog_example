e4a9a10e59fc7c989e0936659c8bd6e2b2328ae0
Victor A Medina <victor.a.medina@boeing.com>
2020-12-31 08:56:08 -0800

    forms.py edits

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -6,3 +6,14 @@ class PostForm(forms.ModelForm):
     class Meta():
         model = Post
         fields = ('author', 'title', 'text')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'})
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta():
+        model = Comment
+        fields = ('author','text')
