6468c87c1c607ff45f425cb2f7e3bbc8fa353738
Victor A Medina <victor.a.medina@boeing.com>
2020-12-31 09:11:23 -0800

    updated folders

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -17,3 +17,8 @@ class CommentForm(forms.ModelForm):
     class Meta():
         model = Comment
         fields = ('author','text')
+
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'})
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+        }
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,'/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
